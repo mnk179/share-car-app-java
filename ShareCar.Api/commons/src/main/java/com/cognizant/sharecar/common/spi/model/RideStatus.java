24e4a29bedb171b3dc847c69ba08a4cde97e819d
mnk179 <martynas.nor@gmail.com>
2018-07-24 21:40:20 +0300

    Adds RideStatus and TripStatus enums

--- ShareCar.Api/commons/src/main/java/com/cognizant/sharecar/common/spi/model/RideStatus.java
+++ ShareCar.Api/commons/src/main/java/com/cognizant/sharecar/common/spi/model/RideStatus.java
@@ -1,4 +1,11 @@
 package com.cognizant.sharecar.common.spi.model;
 
 public enum RideStatus {
+    REQUEST_PENDING,
+    REQUEST_ACCEPTED,
+    REQUEST_DECLINED,
+    REQUEST_CANCELLED,
+    RIDE_CANCELLED,
+    RIDE_SUCCESSFUL,
+    RIDE_REMOVED
 }
--- ShareCar.Api/commons/src/main/java/com/cognizant/sharecar/common/spi/model/TripStatus.java
+++ ShareCar.Api/commons/src/main/java/com/cognizant/sharecar/common/spi/model/TripStatus.java
@@ -1,5 +1,7 @@
 package com.cognizant.sharecar.common.spi.model;
 
 public enum TripStatus {
-
+    SCHEDULED,
+    CANCELLED,
+    SUCCESSFUL
 }
