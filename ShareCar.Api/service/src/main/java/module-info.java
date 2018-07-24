2a323fa02d80e4fe3e99a5be36042f84c7be14a9
Obolevicius <TAOB01@storebrand.com>
2018-07-24 14:07:21 +0300

    New project without components created during workshop added.

---
+++ ShareCar.Api/service/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module service {
+    requires api;
+    requires repository;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires spring.beans;
+    requires spring.context;
+    requires commons;
+}
