7475ba95683c29041f7b7a957f7a3020eaee5494
Raminta-Zurauskaite <ramintules@gmail.com>
2018-07-31 16:07:26 +0300

    Fixed navigation bar button css

--- ShareCar.Client/src/components/NavigationBar/NavBar.jsx
+++ ShareCar.Client/src/components/NavigationBar/NavBar.jsx
@@ -6,11 +6,11 @@ import { Link } from "react-router-dom";
 export class NavBar extends React.Component<{}> {
     render() {
         return (
-            <div className="NavBar">
-            <button className="button">Profile</button>
-            <Link to="/trips"><button className="button">Routes map</button></Link>
-            <button className="button">Rides</button>
-            <button className="button">Change role</button>
+            <div className="bottom-navbar">
+            <button className="bottom-navbar-button">Profile</button>
+            <Link to="/trips"><button className="bottom-navbar-button">Routes map</button></Link>
+            <button className="bottom-navbar-button">Rides</button>
+            <button className="bottom-navbar-button">Change role</button>
             </div>
         );
     }
