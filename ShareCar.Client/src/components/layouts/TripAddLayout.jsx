2125be1742bafdb49b4daf1064b00a5b4d1fa63d
Martynas Noruišis <martynas.nor@gmail.com>
2018-07-31 11:16:07 +0300

    Implements NewTripForm

--- ShareCar.Client/src/components/layouts/TripAddLayout.jsx
+++ ShareCar.Client/src/components/layouts/TripAddLayout.jsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 import { NewTripForm } from "../Trip/NewTripForm";
 import {TripService} from "../../api/TripService";
+import { NavBar } from "./NavBar";
 
 type TripAddLayoutProps = {
     tripService: TripService
@@ -13,6 +14,7 @@ export class TripAddLayout extends React.Component<TripAddLayoutProps> {
     render(){
         return(
             <div>
+                <NavBar/>
                 <NewTripForm tripService={this.props.tripService}/>
             </div>
         );
